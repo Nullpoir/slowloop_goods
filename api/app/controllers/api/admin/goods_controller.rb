c28b036b9a5917c37bca00858e2170cfd67c0411
nullpoir <nullpoir0805@gmail.com>
2025-04-05 16:42:14 +0900

    サーバーサイドの不具合解消

--- api/app/controllers/api/admin/goods_controller.rb
+++ api/app/controllers/api/admin/goods_controller.rb
@@ -1,60 +1,58 @@
-module Api
-  class GoodsController < ApplicationController
-    before_action :good, only: %i(show update destroy)
-    before_action :init_good, only: %i(create)
+class Api::Admin::GoodsController < ApplicationController
+  before_action :good, only: %i(show update destroy)
+  before_action :init_good, only: %i(create)
 
-    def index
-      render json: resources, each_serializer: GoodSerializer
-    end
+  def index
+    render json: resources, each_serializer: GoodSerializer
+  end
 
-    def show
-      render_json
-    end
+  def show
+    render_json
+  end
 
-    def create
-      @good.save!
-      render_json
-    end
+  def create
+    @good.save!
+    render_json
+  end
 
-    def update
-      @good.update!(good_params)
-      render_json
-    end
-    
-    def destroy
-      @good.destroy!
-      render_success_destroy
-    end
+  def update
+    @good.update!(good_params)
+    render_json
+  end
+  
+  def destroy
+    @good.destroy!
+    render_success_destroy
+  end
 
-    private
+  private
 
-    def render_json
-      render json: @good, serializer: GoodSerializer
-    end
+  def render_json
+    render json: @good, serializer: GoodSerializer
+  end
 
-    def resources_scope
-      Good.all
-    end
+  def resources_scope
+    Good.all
+  end
 
-    def init_good
-      @good = Good.new(good_params)
-    end
+  def init_good
+    @good = Good.new(good_params)
+  end
 
-    def good
-      @good = Good.find(params[:id])
-    end
+  def good
+    @good = Good.find(params[:id])
+  end
 
-    def good_params
-      params.require(:good)
-            .permit(
-              :name,
-              :description,
-              :isbn,
-              :jan,
-              :shopping_url,
-              :released_at,
-              :production_ended_at
-            )
-    end
+  def good_params
+    params.require(:good)
+          .permit(
+            :name,
+            :description,
+            :isbn,
+            :jan,
+            :shopping_url,
+            :released_at,
+            :production_ended_at
+          )
   end
 end
